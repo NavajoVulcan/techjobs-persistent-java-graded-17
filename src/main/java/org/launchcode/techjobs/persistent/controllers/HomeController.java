f7196cf149055adce689b90fd2a87e27ae974450
Rico Rose <r.andereck@gmail.com>
2023-11-28 20:04:20 -0600

    optional error intellij?????f

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,7 +1,11 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,6 +21,12 @@ import java.util.Optional;
 @Controller
 public class HomeController {
 
+    //@-attributes are like christmas lights, let the compiler know a special thing to do with the following line
+    @Autowired
+    private EmployerRepository employerRepository;
+    @Autowired
+    private JobRepository jobRepository;
+
     @RequestMapping("/")
     public String index(Model model) {
 
@@ -28,6 +38,7 @@ public class HomeController {
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
 	model.addAttribute("title", "Add Job");
+        model.addAttribute("employers", employerRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
@@ -39,6 +50,13 @@ public class HomeController {
         if (errors.hasErrors()) {
 	    model.addAttribute("title", "Add Job");
             return "add";
+        }
+        else {
+            //Intellij suggested solution, optional and null issue (?) google more
+            //TODO: how do I get an Employer out of the Optional<Employer> that's returned by findById?
+            Employer employer = employerRepository.findById(employerId);
+            newJob.setEmployer(employer);
+            jobRepository.save(newJob);
         }
 
         return "redirect:";
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,13 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Employer extends AbstractEntity {
     public Employer(){
 
     }
+    //step 1, part 3
+    //OneToMany lesson @ https://education.launchcode.org/java-web-dev-curriculum/orm-relationships/reading/one-to-many/index.html
+    //JoinColumn documentation @ https://jakarta.ee/specifications/persistence/2.2/apidocs/javax/persistence/joincolumn
+    @OneToMany()
+    @JoinColumn(name="employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
     public String getLocation() {
         return location;
